914f093397f4676ac93cf4dcdad5d50191c6f0f3
Alex Khokhryakov <a.khokhryakov@unistream.com>
2018-06-30 21:16:01 +0300

    Add owin.

--- SelfHost2/SelfHost2/Program.cs
+++ SelfHost2/SelfHost2/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using  System.Web.Http.Common;
+
 
 namespace SelfHost2
 {
@@ -10,6 +14,19 @@ namespace SelfHost2
     {
         static void Main(string[] args)
         {
+            //var container = new WindsorContainer();
+            //container.Install(new AppInstaller());
+
+            //HttpSelfHostConfiguration config = new HttpSelfHostConfiguration("http://localhost:56785");
+            //config.Routes.MapHttpRoute(
+            //    name: "Default",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    constraints: new
+            //    {
+            //        id = "^[12]$"
+            //    },
+            //    defaults: new { Id = RouteParameter.Optional }
+            //);
         }
     }
 }
