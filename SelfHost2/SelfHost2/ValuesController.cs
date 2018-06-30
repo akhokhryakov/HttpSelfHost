66a50d06cea852a184b14050a2e28ce2dcffed04
Alex Khokhryakov <a.khokhryakov@unistream.com>
2018-06-30 23:17:58 +0300

    Add controller

---
+++ SelfHost2/SelfHost2/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SelfHost2
+{
+    public class ValuesController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5 
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
+        }
+    }
+}
